e78246454a2589a6ad6718cd8a269fc83bef6428
Mike Smiley <msmiley@alumni.rice.edu>
2017-09-25 14:21:40 -0400

    add debug

--- src/handler.js
+++ src/handler.js
@@ -2,19 +2,47 @@
 // Base class for Struts Handler
 //
 class Handler {
-  
+  //
+  // Constructor accepts reference to emitter. The emitter is central to handler
+  // operation and should have can be any implementation as long as it adheres
+  // to `EventEmitter` convention.
+  // @param emitter The emitter to use for this handler
+  //
   constructor(emitter) {
     this.emitter = emitter;
   }
   
   //
-  // Log message handler
+  // If no message is provided, enable debug on this handler, otherwise
+  // this function will emit a log event if debug is enabled.
+  //
+  debug(msg) {
+    if (msg === undefined) {
+      // no argument, enable debug
+      this._isDebug = true;
+    } else {
+      // log only if debug was ever enabled
+      if (this._isDebug) {
+        this.emitter.emit('log', {
+          src: this.constructor.name,
+          lvl: 'debug',
+          msg: msg
+        });
+      }
+    }
+    return this;
+  }
+
+  //
+  // Standard log message handler
   //
   log(msg) {
     this.emitter.emit('log', {
       src: this.constructor.name,
+      lvl: 'normal',
       msg: msg
     });
+    return this;
   }
   
   //
@@ -23,9 +51,13 @@ class Handler {
   error(err) {
     this.emitter.emit('error', {
       src: this.constructor.name,
+      lvl: 'error',
       msg: err
     });
+    return this;
   }
+  
+  
 }
 
 //

5e4eaaeb769652d62828d2d962726e3d3d3f1792
Mike Smiley <msmiley@alumni.rice.edu>
2017-09-25 14:33:40 -0400

    global debug enable, options object

--- src/handler.js
+++ src/handler.js
@@ -7,9 +7,11 @@ class Handler {
   // operation and should have can be any implementation as long as it adheres
   // to `EventEmitter` convention.
   // @param emitter The emitter to use for this handler
+  // @param options A freeform object specifying options used by derived class
   //
-  constructor(emitter) {
+  constructor(emitter, options={}) {
     this.emitter = emitter;
+    this.options = options;
     
     // call init() if it is defined. This is a struts.Handler user convenience
     // to prevent having to call super() in the derived class constructor. The user
@@ -17,6 +19,9 @@ class Handler {
     if (this.init && typeof(this.init) === 'function') {
       this.init();
     }
+    
+    // handle global struts debug enable event
+    this.emitter.on('struts.debug', () => this.debug());
   }
   
   //
@@ -30,7 +35,7 @@ class Handler {
     } else {
       // log only if debug was ever enabled
       if (this._isDebug) {
-        this.emitter.emit('log', {
+        this.emitter.emit('struts.log', {
           lvl: 'debug',
           src: this.constructor.name,
           msg: msg
@@ -44,7 +49,7 @@ class Handler {
   // Standard log message handler
   //
   log(msg) {
-    this.emitter.emit('log', {
+    this.emitter.emit('struts.log', {
       lvl: 'normal',
       src: this.constructor.name,
       msg: msg
@@ -56,6 +61,8 @@ class Handler {
   // error handler
   //
   error(err) {
+    // keep this event as 'error' to take advantage of the native checks that
+    // make sure the event is handled.
     this.emitter.emit('error', {
       lvl: 'error',
       src: this.constructor.name,
